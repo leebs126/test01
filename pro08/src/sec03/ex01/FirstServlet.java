08d3cb6af3fdeb56539bf277ba70ec0e97773995
leebs <leebs@DESKTOP-3FAS477>
2018-11-21 16:34:57 +0900

    소스 코드 업로드함

---
+++ pro08/src/sec03/ex01/FirstServlet.java
@@ -0,0 +1,21 @@
+package sec03.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*@WebServlet("/first")*/
+public class FirstServlet extends HttpServlet{
+   public void doGet(HttpServletRequest request, HttpServletResponse response) 
+                                                                 throws IOException, ServletException {
+      response.setContentType("text/html;charset=utf-8");
+      RequestDispatcher dispatch = request.getRequestDispatcher("second?name=lee");  
+      dispatch.forward(request, response);
+   }
+}
+
